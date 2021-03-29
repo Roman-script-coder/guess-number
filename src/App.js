ed5b8ae1a9b8be95c324d9f37d9ff2af7a1fbc06
Roman <Belec93@mail.ru>
2021-03-29 23:22:07 +0300

    New component GameField.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { GameField } from "./GameField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="profilePlayer">
+		    <span>Правила игры:<br/>
+		    - Введите свое имя;<br/>
+		    - Компьютер выбирает случайное число от 1 до 100. Игроку необходимо вводить число, пытаясь найти, какое же число
+           выбрал компьютер.</span><br/><br/>
+        <input className="playerName" maxLength="17" />
+        <button onClick="enterPlayerName()">Ввести имя</button>
+      </div>
+      <GameField />
+    </>
   );
 }
 
---
+++ src/GameField/GameField.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+export const GameField = () => {
+  return (
+    <div className="gameField">
+      <input className="enterGuessNumber" />
+      <button onClick="enterNumber()">Ввести число</button>
+      <div className="informationField">
+        <div className="answersMore" />
+        <div className="answersLess" />
+        <div className="answerRight" />
+      </div>
+      <div className="overallResult">
+        <span>Имя игрока:</span>
+        <span>Количество попыток игрока:</span>
+      </div>
+    </div>
+  );
+}
